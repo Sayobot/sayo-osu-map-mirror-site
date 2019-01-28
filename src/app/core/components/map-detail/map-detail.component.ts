e1e98895cc96ec0b298f29a7989e6a8b477f545f
souldee <17746076034@163.com>
2019-01-28 22:53:26 +0800

    修改播放音频域名，增加web更新历史

--- src/app/core/components/map-detail/map-detail.component.ts
+++ src/app/core/components/map-detail/map-detail.component.ts
@@ -91,7 +91,6 @@ export class MapDetailComponent implements OnInit {
     ngOnInit() {
         this.imgUrl = `https://cdn.sayobot.cn:25225/beatmaps/${this.data.id}/covers/cover.jpg?0`;
         this.mapDetail = this.data.content;
-        this.musicBox.setSrc(`https://cdn.sayobot.cn:25225/preview/${this.data.id}.mp3`);
+        this.musicBox.setSrc(`https://cdnx.sayobot.cn:25225/preview/${this.data.id}.mp3`);
     }
-
 }
--- src/app/core/components/update-log-dialog/update-log-dialog.component.ts
+++ src/app/core/components/update-log-dialog/update-log-dialog.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { META } from './update,component.Meta';
 
 @Component({
-  selector: 'app-update-log-dialog',
-  templateUrl: './update-log-dialog.component.html',
-  styleUrls: ['./update-log-dialog.component.scss']
+    selector: 'app-update-log-dialog',
+    templateUrl: './update-log-dialog.component.html',
+    styleUrls: ['./update-log-dialog.component.scss']
 })
 export class UpdateLogDialogComponent implements OnInit {
 
-  constructor() { }
+    updataList: any;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.updataList = META;
+    }
 
 }
