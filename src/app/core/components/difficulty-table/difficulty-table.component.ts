d359b25bef88d13114454cbe1f6e136bc3fb43ff
SoulDee <SoulDee@users.noreply.github.com>
2019-04-14 17:44:31 +0800

    feat: 右上角增加难度系数表

--- src/app/core/components/difficulty-table/difficulty-table.component.ts
+++ src/app/core/components/difficulty-table/difficulty-table.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'difficulty-table',
-  templateUrl: './difficulty-table.component.html',
-  styleUrls: ['./difficulty-table.component.scss']
+    selector: 'difficulty-table',
+    templateUrl: './difficulty-table.component.html',
+    styleUrls: ['./difficulty-table.component.scss']
 })
 export class DifficultyTableComponent implements OnInit {
+    @Input() maps: any;
+    @Output() difficultChange: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+    currentIndex = 0;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+
+    }
+
+    select(index: number) {
+        this.currentIndex = index;
+        this.difficultChange.emit(index);
+    }
+
+    getStar(star: number): string {
+        return star.toFixed(2);
+    }
 }
