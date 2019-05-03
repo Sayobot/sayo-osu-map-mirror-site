2cb2559967b56baf4007aa989b3a701157a0a5ca
SoulDee <SoulDee@users.noreply.github.com>
2019-05-03 15:17:34 +0800

    feat：增加音乐盒组件 待完善

---
+++ src/app/home/music-box/music-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'music-box',
+    templateUrl: './music-box.component.html',
+    styleUrls: ['./music-box.component.scss']
+})
+export class MusicBoxComponent implements OnInit {
+
+    // 饥人谷 音乐参考 https://github.com/jirengu/music
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
