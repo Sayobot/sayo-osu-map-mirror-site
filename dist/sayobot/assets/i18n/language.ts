be0191327fcd1f03c49539fe891e2926fa7cc269
souldee <17746076034@163.com>
2020-03-07 13:05:16 +0800

    dist: update dist

--- dist/sayobot/assets/i18n/language.ts
+++ dist/sayobot/assets/i18n/language.ts
@@ -1,15 +1,17 @@
-
-export const language = [{
-    title: '简体中文',
-    key: 'zh',
-    flag: '/assets/img/flags/CN.png',
-}, {
-    title: 'English',
-    key: 'en',
-    flag: '/assets/img/flags/GB.png',
-}, {
-    title: '日本語',
-    key: 'jp',
-    flag: '/assets/img/flags/JP.png',
-}]
-
+export const language = [
+    {
+        title: '简体中文',
+        key: 'zh',
+        flag: '/assets/img/flags/CN.png'
+    },
+    {
+        title: 'English',
+        key: 'en',
+        flag: '/assets/img/flags/GB.png'
+    },
+    {
+        title: '日本語',
+        key: 'jp',
+        flag: '/assets/img/flags/JP.png'
+    }
+];
