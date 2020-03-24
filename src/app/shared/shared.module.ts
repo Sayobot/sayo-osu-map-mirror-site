fedcf831635718640862c6069cef9fd1ea12a708
SoulDee <SoulDee@users.noreply.github.com>
2020-03-24 20:27:20 +0800

    feat: add scroll-top component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,6 @@ import { COMPONENTS, MODULES } from './shared.meta';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { MapSearchOptionsComponent } from './components/map-search-options/map-search-options.component';
-import { MapSearchResultItemComponent } from './components/map-search-result-item/map-search-result-item.component';
-import { MapSearchQuickbarComponent } from './components/map-search-quickbar/map-search-quickbar.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
@@ -23,7 +20,7 @@ export function HttpLoaderFactory(http: HttpClient) {
             }
         })
     ],
-    declarations: [...COMPONENTS, MapSearchOptionsComponent, MapSearchResultItemComponent, MapSearchQuickbarComponent],
+    declarations: [...COMPONENTS],
     exports: [...MODULES, ...COMPONENTS]
 })
 export class SharedModule {}
