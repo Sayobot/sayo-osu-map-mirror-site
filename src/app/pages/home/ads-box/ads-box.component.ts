d4f491ccb63d787447203b9bdbfe2df8dfb24793
SoulDee <SoulDee@users.noreply.github.com>
2020-04-22 23:42:57 +0800

    fix: 去除广告栏翻页，添加详情的关闭按钮，i18n

--- src/app/pages/home/ads-box/ads-box.component.ts
+++ src/app/pages/home/ads-box/ads-box.component.ts
@@ -48,11 +48,11 @@ export class AdsBoxComponent implements OnInit, AfterViewInit, OnDestroy {
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
             }
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev'
+            // }
         });
     }
 
